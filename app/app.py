df387e06e64925e797410564b2787b646087789d
mikanbearer <50346402+mikanbearer@users.noreply.github.com>
2020-09-15 10:19:53 +0800

    append login

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import flask_login
 from datetime import datetime
-
 
 
 app = Flask(__name__)
@@ -10,7 +10,56 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db?check_same_thread=Fals
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+admins = {'testadmin': {'password': '123'}}
+
+class Admin(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(admin_name):
+    if admin_name not in admins:
+        return
+    admin = Admin()
+    admin.id = admin_name
+    return admin
+
+@login_manager.request_loader
+def request_loader(request):
+    admin_name = request.form.get('user')
+    if admin_name not in admins:
+        return
+    admin = Admin()
+    admin.id = admin_name
+    admin.is_authenticated = request.form['password'] == admins[admin_name]['password']
+    return admin
+
+@app.login_manager.unauthorized_handler
+def unauth():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        admin_name = request.form['user']
+        try:
+            if request.form['password'] == admins[admin_name]['password']:
+                admin = Admin()
+                admin.id = admin_name
+                flask_login.login_user(admin)
+                return redirect(url_for('list'))
+        except:
+            return render_template('login.html', error='Login Failed')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect(url_for('login'))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,18 +71,20 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
 @app.route('/')
+@flask_login.login_required
 def list():
     users = User.query.all()
     return render_template('list.html', users=users)
 
 @app.route('/detail/<user_id>')
+@flask_login.login_required
 def detail(user_id):
     user = User.query.filter_by(id=user_id).first()
     return render_template('detail.html', user=user)
 
 @app.route('/create', methods=['get', 'post'])
+@flask_login.login_required
 def create():
     if request.method == 'POST':
         username = request.form['username']
@@ -45,6 +96,7 @@ def create():
     return render_template('form.html')
 
 @app.route('/update/<user_id>', methods=['get', 'post'])
+@flask_login.login_required
 def update(user_id):
     user = User.query.filter_by(id=user_id).first()
     if request.method == 'POST':
@@ -56,6 +108,7 @@ def update(user_id):
     return render_template('form.html', user=user)
 
 @app.route('/delete/<user_id>', methods=['get', 'post'])
+@flask_login.login_required
 def delete(user_id):
     user = User.query.filter_by(id=user_id).first()
     if request.method == 'POST':
